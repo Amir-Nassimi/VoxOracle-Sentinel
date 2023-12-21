c5d5b23bbf2beb8825d6aef0bc6ff0e0013ceac2
a.nasimi <>
2023-12-21 19:01:31 +0330

    reFactor gender_bas..:Optimize the source code

--- Audio Processing/gender_based_pitch_shifter.py
+++ Audio Processing/gender_based_pitch_shifter.py
@@ -30,51 +30,52 @@ class AudioFileProcessor:
 
 @singleton
 class PitchShifter:
-    def __init__(self):
-        self._min_shift = 0.2
-        self._max_shift = 1.2
-    
-    @property
-    def min_shift(self):
-        return self._min_shift
-    
-    @min_shift.setter
-    def min_shift(self, value):
-        self._min_shift = value
+    def __init__(self, min_shift, max_shift, male_sff, female_sff):
+        self.min_shift = min_shift
+        self.max_shift = max_shift
 
-    @property
-    def max_shift(self):
-        return self._max_shift
-    
-    @max_shift.setter
-    def max_shift(self, value):
-        self._max_shift = value
+        self.male_sff = male_sff
+        self.female_sff = female_sff
 
     def calculate_sff(self, audio, sr):
         pitches, magnitudes = librosa.piptrack(y=audio, sr=sr)
         pitch = np.max(pitches)
         return pitch
 
+    def get_unique_shift_amount_sff(self, gender, sff, aug_index, num_augmentations):
+        print(f"Calculating shift amount: Gender: {gender}, SFF: {sff}, Index: {aug_index}, Num Augmentations: {num_augmentations}")
+        if gender == 'male' and sff >= self.male_sff:
+            shift_range = np.linspace(self.min_shift, self.max_shift, num_augmentations)
+        elif gender == 'female' and sff < self.female_sff:
+            shift_range = np.linspace(-self.max_shift, -self.min_shift, num_augmentations)
+        else:
+            return 0  # No shift for androgynous or already within target range
+
+        return shift_range[aug_index]
+    
     def get_unique_shift_amount(self, gender, sff, aug_index, num_augmentations):
         print(f"Calculating shift amount: Gender: {gender}, SFF: {sff}, Index: {aug_index}, Num Augmentations: {num_augmentations}")
-        if gender == 'male' and sff >= 130:
+        if gender == 'male':
             shift_range = np.linspace(self.min_shift, self.max_shift, num_augmentations)
-        elif gender == 'female' and sff < 180:
+        elif gender == 'female':
             shift_range = np.linspace(-self.max_shift, -self.min_shift, num_augmentations)
         else:
             return 0  # No shift for androgynous or already within target range
 
         return shift_range[aug_index]
 
-class Executer:
-    def __init__(self, input_dir, output_dir, sample_rate, num_augmentations, thrsh):
-        self.thrsh = thrsh
-        self.input_dir = input_dir
-        self.output_dir = output_dir
-        self.sample_rate = sample_rate
-        self.num_augmentations = num_augmentations
 
-        self.shifter = PitchShifter()
+class Executer:
+    def __init__(self, **kwargs):
+        self.thrsh = kwargs.get("thrsh")
+        self.input_dir = kwargs.get("input_dir")
+        self.output_dir = kwargs.get("output_dir")
+        self.sample_rate = kwargs.get("sample_rate")
+        self.num_augmentations = kwargs.get("num_augmentations")
+
+        self.use_sff = kwargs.get("use_sff")
+
+        self.shifter = PitchShifter(kwargs.get("min_shift"), kwargs.get("max_shift"), kwargs.get("male_sff"), kwargs.get("female_sff"))
         self.detector = GenderDetector()
 
     def process_directory(self):
@@ -96,7 +97,10 @@ class Executer:
                     else:
                         segment_audio = processor.audio
                     sff = self.shifter.calculate_sff(segment_audio, processor.sample_rate)
-                    shift_amount = self.shifter.get_unique_shift_amount(label, sff, i, self.num_augmentations)
+
+                    if self.use_sff: shift_amount = self.shifter.get_unique_shift_amount_sff(label, sff, i, self.num_augmentations)
+                    else: shift_amount = self.shifter.get_unique_shift_amount(label, sff, i, self.num_augmentations)
+
                     shifted_segment = processor.apply_pitch_shift(start, end, shift_amount, self.thrsh)
                     processed_audio.append(shifted_segment)
 
@@ -115,20 +119,22 @@ class Executer:
 
 def main():
     parser = argparse.ArgumentParser(description="Audio processing with gender detection and pitch shifting.")
+    
+    parser.add_argument("--male_sff", required=False, type=float, default=130, help="Male SFF threshold.")
+    parser.add_argument("--female_sff", required=False, type=float, default=180, help="Female SFF threshold.")
+    parser.add_argument("--use_sff", required=False, action='store_false', help="Do not use SFF values for pitch shifting.")
 
     parser.add_argument("--output_dir", required=True, help="Output directory for processed audio.")
     parser.add_argument("--input_dir", required=True, help="Input directory containing audio files.")
-    parser.add_argument("--min_shift", required=False, type=float, default=1.2, help="Minimum pitch shift factor.")
+    parser.add_argument("--min_shift", required=False, type=float, default=-3, help="Minimum pitch shift factor.")
     parser.add_argument("--max_shift", required=False, type=float, default=3.5, help="Maximum pitch shift factor.")
     parser.add_argument("--sample_rate", required=False, type=int, default=16000, help="Sample rate for audio processing.")
     parser.add_argument("--num_augmentations", required=False, type=int, default=1, help="Number of augmentations per audio file.")
-    parser.add_argument("--thrsh", required=False, type=bool, default=False, help="To Completely Augment (value:False) or Only change the part (value:True) in which the speaker speaks.")
+    parser.add_argument("--thrsh", required=False, action='store_false', help="To Completely Augment (value:False) or Only change the part (value:True) in which the speaker speaks.")
 
     args = parser.parse_args()
 
-    executer = Executer(args.input_dir, args.output_dir, args.sample_rate, args.num_augmentations, args.thrsh)
-    executer.shifter.min_shift = args.min_shift
-    executer.shifter.max_shift = args.max_shift
+    executer = Executer(**vars(args))
     executer.process_directory()
 
 if __name__ == "__main__":
