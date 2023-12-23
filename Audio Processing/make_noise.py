79e89e0a2e70d289ad8a5fb9dc3f801e09119f7d
a.nasimi <>
2023-12-23 09:32:17 +0330

    Add make_noise

---
+++ Audio Processing/make_noise.py
@@ -0,0 +1,71 @@
+import os
+import librosa
+import argparse
+import numpy as np
+import glob
+from multiprocessing import Pool
+
+class AudioProcessor:
+    def __init__(self, input_dirs, output_base_dir, input_length, sample_rate, noise_factor):
+        self.input_dirs = input_dirs
+        self.output_base_dir = output_base_dir
+        self.input_length = input_length
+        self.sample_rate = sample_rate
+        self.noise_factor = noise_factor
+
+    def get_output_path(self, input_path):
+        # Find the base input directory that is a parent of the input path
+        for input_dir in self.input_dirs:
+            if input_path.startswith(input_dir):
+                # Replace the base input directory with the base output directory
+                relative_path = os.path.relpath(input_path, input_dir)
+                return os.path.join(self.output_base_dir, relative_path)
+        # Default case if no matching input directory found
+        return os.path.join(self.output_base_dir, os.path.basename(input_path))
+
+    def process_single_audio(self, audio_path):
+        try:
+            audio, sr = librosa.load(audio_path, sr=self.sample_rate)
+
+            if len(audio) < int(self.input_length * self.sample_rate):
+                total_padding = int(self.input_length * self.sample_rate) - len(audio)
+                padding_start = total_padding // 2
+                padding_end = total_padding - padding_start
+                audio = np.pad(audio, (padding_start, padding_end), 'constant')
+            else:
+                audio = audio[:int(self.input_length * self.sample_rate)]
+
+            noisy_audio = audio * self.noise_factor
+            output_path = self.get_output_path(audio_path)
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+            librosa.output.write_wav(output_path, noisy_audio, sr)
+            print(f"Processed: {audio_path}")
+
+        except Exception as e:
+            print(f"Error processing {audio_path}: {str(e)}")
+
+    def process_audio(self):
+        pool = Pool()
+        audio_paths = []
+
+        for input_dir in self.input_dirs:
+            audio_paths.extend(glob.glob(os.path.join(input_dir, '**', '*.wav'), recursive=True))
+
+        pool.map(self.process_single_audio, audio_paths)
+        pool.close()
+        pool.join()
+
+def main():
+    parser = argparse.ArgumentParser(description="Audio processing script")
+    parser.add_argument("input_dirs", nargs='+', help="Input directories containing audio files.")
+    parser.add_argument("--output_base_dir", required=True, help="Base output directory for processed audio.")
+    parser.add_argument("--input_length", type=float, required=True, help="Desired input length in seconds.")
+    parser.add_argument("--sample_rate", type=int, required=False, default=16000, help="Sample rate for audio.")
+    parser.add_argument("--noise_factor", type=float, default=0.1, help="Multiplication factor for noise.")
+    args = parser.parse_args()
+
+    audio_processor = AudioProcessor(args.input_dirs, args.output_base_dir, args.input_length, args.sample_rate, args.noise_factor)
+    audio_processor.process_audio()
+
+if __name__ == "__main__":
+    main()
