e742343f0a9f8690304123c98ad99a495933da20
a.nasimi <>
2023-12-24 09:11:40 +0330

    reFactor source code: add modules to proc. recursive audio directories; Ensure the mixed audio does not exceed -1 to 1 range, reFactor source code

--- Data Preparation/random_audio_mixer.py
+++ Data Preparation/random_audio_mixer.py
@@ -2,11 +2,19 @@ import os
 import random
 import librosa
 import argparse
+import numpy as np
+from glob import glob
 import soundfile as sf
 
 
 def get_audio_files(directory):
+    # For directories without a specific pattern
     return [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.wav')]
+
+
+def get_audio_files_recursive(directory):
+    return glob(os.path.join(directory, '**', '*.wav'), recursive=True)
+
 
 def process_audio(file1, file2, output_dir, sample_rate):
     # Load both audio files with the specified sample rate
@@ -26,6 +34,9 @@ def process_audio(file1, file2, output_dir, sample_rate):
     # Add the audio files
     mixed_audio = audio1 + audio2
 
+    # Ensure the audio does not exceed -1 to 1 range
+    mixed_audio = np.clip(mixed_audio, -1, 1)
+
     # Save the mixed audio
     output_filename = os.path.basename(file1).replace('.wav', '_mixed.wav')
     output_path = os.path.join(output_dir, output_filename)
@@ -33,9 +44,10 @@ def process_audio(file1, file2, output_dir, sample_rate):
 
     return True
 
+
 def mix_audios(folder1, folder2, output_dir, sample_rate):
-    files1 = get_audio_files(folder1)
-    files2 = get_audio_files(folder2)
+    files1 = get_audio_files_recursive(folder1)  # Using the recursive function for source 1
+    files2 = get_audio_files(folder2)  # Using the original function for source 2
 
     # Shuffle the lists to ensure random selection
     random.shuffle(files1)
@@ -49,6 +61,7 @@ def mix_audios(folder1, folder2, output_dir, sample_rate):
             file1 = files1.pop(random.randint(0, len(files1) - 1))
             success = process_audio(file1, file2, output_dir, sample_rate)
 
+
 def main():
     parser = argparse.ArgumentParser(description="Mix audios from two folders.")
 
@@ -61,6 +74,7 @@ def main():
 
     mix_audios(args.folder1, args.folder2, args.output_dir, args.sample_rate)
     print(f"Audio mixing complete. Mixed files saved in '{args.output_dir}'.")
+
 
 if __name__ == "__main__":
     main()
